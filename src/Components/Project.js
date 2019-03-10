a30f28474f2e79146c6abcb1385d15363a57cea4
Mingchao Zhang <mz5@illinois.edu>
2019-03-10 17:35:30 -0500

    add title

---
+++ src/Components/Project.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Projects extends Component {
+  render() {
+    return (
+      <div className="Projects">
+        Projects!!!
+      </div>
+    );
+  }
+}
+
+export default Projects;
