67c85d2e1968c8391b43d148f03384e5469812da
Mingchao Zhang <mz5@illinois.edu>
2019-03-11 02:28:34 -0500

    done with search box styling

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import Navbar from '../layout/Navbar';
+import './Search.scss'
 
 function Search() {
     return (
         // a wrapper to include all elements since we can only return one global element   
         <React.Fragment>
             <Navbar />
-            <input placeholder="Enter the movie name" style  = {{
-                  marginTop: '5px',
-                  padding: '5px',
-                  display: 'block',
-                  fontSize: '1rem',
-                  width: '100%',
-                  textAlign: 'center'
-                }}/>
+            <input placeholder="Enter the movie name" className="inputBox"/>
             <h1>Movie Search</h1>
         </React.Fragment>
     )
