b33deee8797cd3b5e198e0dbe78d5d972b048273
Mingchao Zhang <mz5@illinois.edu>
2019-03-11 20:23:58 -0500

    making progress

--- src/components/pages/Gallery.js
+++ src/components/pages/Gallery.js
@@ -5,21 +5,7 @@ import OptionBar from '../layout/OptionBar';
 import './Gallery.scss'
 //import { generateKeyPair } from 'crypto';
 
-class GenreItemDisplay extends Component {
-    render() {
-        return <div className="genreItem">{ this.props.genreName }</div>
-    }
-    
-}
-
-class GenreItemsDisplay extends Component {
-    render() {
-        return (this.props.genres.map((genre) => (
-        <GenreItemDisplay key={genre.id} genreName={genre.name} />))
-        )
-    }
-}
-
+//--------------------------------------------------------
 class Gallery extends Component  {
     GenreItemsDisplay = () => {
         return (this.props.genres.map((genre) => (
@@ -34,13 +20,57 @@ class Gallery extends Component  {
             <Navbar />
             <OptionBar />
             <div className="genreList">
-            <GenreItemsDisplay genres={this.props.genres} />
+                <div className="genreItem"><p>All</p></div>
+                <GenreItemsDisplay genres={this.props.genres} />
             </div>
-            <h1>Movie Gallery</h1>
-
+            <div className="galleryMovieList">
+                <GalleryMoviesDisplay movies={this.props.movies} />
+            </div>
         </React.Fragment>
     )
     }
 }
 
+//--------------------------------------------------------
+class GenreItemDisplay extends Component {
+    render() {
+        return <div className="genreItem">
+                    <p>{ this.props.genreName }</p>
+                </div>
+    }
+    
+}
+
+//--------------------------------------------------------
+class GenreItemsDisplay extends Component {
+    render() {
+        return (this.props.genres.map((genre) => (
+                <GenreItemDisplay key={genre.id} genreName={genre.name} />
+        ))
+        )
+    }
+}
+
+//movie prefex: https://image.tmdb.org/t/p/w185/
+//--------------------------------------------------------
+class GalleryMovieDisplay extends Component {
+    render() {
+        const movie = this.props.movie;
+        const movie_src = "https://image.tmdb.org/t/p/w185/" + movie.poster_path;
+        return <div className="galleryMoviePic">
+                    <img src={movie_src} alt={movie.title} />
+                </div>
+    }
+}
+
+//--------------------------------------------------------
+class GalleryMoviesDisplay extends Component {
+    render() {
+        return (this.props.movies.map((movie) => (
+                <GalleryMovieDisplay key={movie.id} movie={movie} />
+        ))
+        )
+    }
+}
 export default Gallery;
+
